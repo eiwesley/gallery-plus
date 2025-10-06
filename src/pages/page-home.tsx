1377fe125678b2de81e11bf2b0231aa624254268
Wesley Medeiros <Wesley.coelho@outlook.com>
2025-10-06 16:20:06 -0300

    Retornando lista de fotos

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -2,9 +2,11 @@ import Container from '../components/container'
 import AlbumsFilter from '../contexts/albums/components/albums-filter'
 import useAlbums from '../contexts/albums/hooks/useAlbums'
 import PhotosList from '../contexts/photos/components/photos-list'
+import usePhotos from '../contexts/photos/hooks/use-photos'
 
 export default function PageHome() {
   const { albums, isLoadingAlbums } = useAlbums()
+  const { photos, isLoadingPhotos } = usePhotos()
 
   return (
     <Container>
@@ -13,30 +15,7 @@ export default function PageHome() {
         loading={isLoadingAlbums}
         className="mb-9"
       />
-      <PhotosList
-        photos={[
-          {
-            id: '123',
-            title: 'Olá Mundo',
-            imageId: 'portrait-tower.png',
-            albums: [
-              { id: '3421', title: 'Album 1' },
-              { id: '1234', title: 'Album 2' },
-              { id: '5678', title: 'Album 3' },
-            ],
-          },
-          {
-            id: '456',
-            title: 'Olá Mundo',
-            imageId: 'portrait-tower.png',
-            albums: [
-              { id: '3421', title: 'Album 1' },
-              { id: '1234', title: 'Album 2' },
-              { id: '5678', title: 'Album 3' },
-            ],
-          },
-        ]}
-      />
+      <PhotosList photos={photos} loading={isLoadingPhotos} />
     </Container>
   )
 }
