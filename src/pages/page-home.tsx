1f30293af1278ae0749267a539b56494ade6b39b
Wesley Medeiros <Wesley.coelho@outlook.com>
2025-10-06 11:09:36 -0300

    Mock da lista de fotos

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,13 +1,12 @@
 import Container from '../components/container'
-import PhotoWidget from '../contexts/photos/components/photo-widget'
-import type { Photo } from '../contexts/photos/models/photo'
+import PhotosList from '../contexts/photos/components/photos-list'
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-5 gap-9">
-        <PhotoWidget
-          photo={{
+      <PhotosList
+        photos={[
+          {
             id: '123',
             title: 'Olá Mundo',
             imageId: 'portrait-tower.png',
@@ -16,11 +15,9 @@ export default function PageHome() {
               { id: '1234', title: 'Album 2' },
               { id: '5678', title: 'Album 3' },
             ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: '123',
+          },
+          {
+            id: '456',
             title: 'Olá Mundo',
             imageId: 'portrait-tower.png',
             albums: [
@@ -28,23 +25,9 @@ export default function PageHome() {
               { id: '1234', title: 'Album 2' },
               { id: '5678', title: 'Album 3' },
             ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: '123',
-            title: 'Olá Mundo',
-            imageId: 'portrait-tower.png',
-            albums: [
-              { id: '3421', title: 'Album 1' },
-              { id: '1234', title: 'Album 2' },
-              { id: '5678', title: 'Album 3' },
-            ],
-          }}
-        />
-        <PhotoWidget photo={{} as Photo} loading />
-        <PhotoWidget photo={{} as Photo} loading />
-      </div>
+          },
+        ]}
+      />
     </Container>
   )
 }
